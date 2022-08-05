2d9d97bb5a95cd77ec49718577b05689da2dbfec
Haneunwoo <wozlgks@pusan.ac.kr>
2022-08-05 20:50:48 +0900

    push

---
+++ terraform/variables.tf
@@ -0,0 +1,47 @@
+variable "region" {
+  type        = string
+  description = "Default region for root module"
+  default = "This is Default region"
+}
+
+variable "account_id" {
+  type        = string
+  description = "Default account_id for root module"
+  default = "This is Default account_id"
+}
+
+variable "PartitionKey" {
+  type        = string
+  description = "Default PartitionKey for root module"
+  default = "This is Default PartitionKey"
+}
+
+variable "user_name" {
+  type        = string
+  description = "Default user_name for root module"
+  default = "This is Default user_name"
+}
+
+variable "user_password" {
+  type        = string
+  description = "Default user_password for root module"
+  default = "This is Default user_password"
+}
+
+variable "opensearch_index" {
+  type        = string
+  description = "Default opensearch_index for root module"
+  default = "This is Default opensearch_index"
+}
+
+variable "access_key" {
+  type        = string
+  description = "Default access_key for root module"
+  default = "This is Default access_key"
+}
+
+variable "secret_key" {
+  type        = string
+  description = "Default secret_key for root module"
+  default = "This is Default secret_key"
+}
